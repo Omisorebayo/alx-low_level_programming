ad127b42498387c2c73d8119dd6c360215e56a1c
omisorebayo <omisorebayo3@gmail.com>
2022-09-08 11:42:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - this is the main function puts
- * return: 0
+ * Return: 0
  */
 int main(void)
-
 {
-
 	char text[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(text);
 	return (0);
-}		  
+}
