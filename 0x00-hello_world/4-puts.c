5a608705df8a88ab44c927c589c2e84c33d436cd
omisorebayo <omisorebayo3@gmail.com>
2022-09-08 10:46:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,7 @@ int main(void)
 
 {
 
-	char string[Programming is like building a multilingual puzzle];
-
+	char string[] = "\"Programming is like building a multilingual puzzle";
 	puts(string);
 
 	     return 0;
