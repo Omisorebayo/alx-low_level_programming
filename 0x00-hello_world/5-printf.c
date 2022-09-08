6c19edf0a41c0112e201e2081a5fb76376c9bd8d
omisorebayo <omisorebayo3@gmail.com>
2022-09-08 12:12:46 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that prints the size of various types on the computer
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
