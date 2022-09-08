771f8d50a01e3ea6cf4d2df526418015ce9bc1a4
omisorebayo <omisorebayo3@gmail.com>
2022-09-08 10:25:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * this is how to use puts
- * Return:0
+ * main - this is the main function
+ * description - the function to use is puts
  */
 int main(void)
 
 {
+
 	puts("Programming is like building a multilingual puzzle\n");
 
 	     return 0;
