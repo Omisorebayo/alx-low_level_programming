182d8c8699617e258d9af5bfcdae5cde878b5e14
omisorebayo <omisorebayo3@gmail.com>
2022-09-10 03:33:44 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints out a to z except q and e.
+ * Return: 0
+ **/
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (!(n == 'q' || n == 'e'))
+		{
+		putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
